9569cb831a216620929670bda7bdf85b63288242
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-06-03 03:41:32 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,8 +1,7 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  
-(m.chat, await (await fetch(fla + 'List Jadi Bot')).buffer(), `🔧 *Owner Botz*\n\nhttps://wa.me/6282160268332 (David)\n\n⚙️ *Bot Utama* \n\n• https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🔗 Jadi Bot* \n\n` + users.map(v => '• https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name}).join('\n'))
-`.trim(), '*_One Botz_*', 'Pemilik Bot', '#owner')
+  m.reply(`🐤 *Owner Botz*\n\nhttps://wa.me/6282160268332\n\n🐦 *Bot Utama* \n\n https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🗿 JadiBot* \n\n` + users.map(v => '✾ https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+}
 handler.command = handler.help = ['listjadibot', 'listbot', 'ljb']
 handler.tags = ['jadibot']
 
