4b6962534ac97997405267701aedd9320a114f57
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-06-02 20:30:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
 conn.version =[2,2143,3]
-conn.browserDescription =['One Botz', 'Safari', '3.5']
+conn.browserDescription =['One Botz', 'IOS', '3.5']
 let authFile = `${opts._[0] || 'OneBotZ'}.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
