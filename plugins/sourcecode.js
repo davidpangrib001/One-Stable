1d68c53295436eaa175715ea69cbaf8dab82d15a
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-06-06 18:52:54 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'SCRIPT')).buffer(), `
 SCRIPT BOT
-`.trim(), 'https://github.com/davidpangrib001/One.V4', 'Pemilik Bot', '#owner')
+`.trim(), 'https://github.com/davidpangrib001/One-Stable', 'Pemilik Bot', '#owner')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
