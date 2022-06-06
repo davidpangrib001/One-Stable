202370ae4e8058aa8c9bbf3019cc85f4117a45b4
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-06-06 13:32:27 +0700

    Update anti-toxic.js

--- plugins/anti-toxic.js
+++ plugins/anti-toxic.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
-let stc = fs.readFileSync('./src/uh.mp3')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*ADA JAGOAN JADI NGERI*', 'status@broadcast')
+let stc = fs.readFileSync('./src/media_audio_meme-de-creditos-finales.mp3')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Jangan Toxic Bang..*', 'status@broadcast')
 }
 handler.customPrefix = /anjing|memek|kontol|ngentod|goblok|goblog/i
 handler.command = new RegExp
